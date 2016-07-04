bd71bddfc44a80aa1411683c962fc219d3fb520c
li_xuguang <li_xuguang@cdv.com>
2016-07-05 00:33:24 +0800

    ww

---
+++ QxThread.cpp
@@ -0,0 +1,7 @@
+#include "QxThread.h"
+
+QxThread::QxThread()
+{
+
+}
+
---
+++ QxThread.h
@@ -0,0 +1,11 @@
+#ifndef QXTHREAD_H
+#define QXTHREAD_H
+
+
+class QxThread : public QThread
+{
+public:
+    QxThread();
+};
+
+#endif // QXTHREAD_H
